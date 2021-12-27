9e9bf9940df12bae7f0d047a89761219eeeffdbe
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2021-12-26 21:41:12 -0300

    Perfil

--- src/mainMenu/MainMenuPanel.java
+++ src/mainMenu/MainMenuPanel.java
@@ -3,7 +3,7 @@ package mainMenu;
 import javax.swing.*;
 
 public class MainMenuPanel extends JPanel{
-	JButton jbInicio,jbNotas,jbSair;
+	JButton jbInicio,jbNotas,jbPerfil,jbSair;
 	
 	public MainMenuPanel() {
 		jbInicio=new JButton("Início");
@@ -14,14 +14,18 @@ public class MainMenuPanel extends JPanel{
 		jbNotas.setBounds(100,100,100,30);
 		jbNotas.addActionListener(new MainMenuPanelButtonsActionListener(1));
 		
+		jbPerfil=new JButton("Perfil");
+		jbPerfil.setBounds(100,100,100,30);
+		jbPerfil.addActionListener(new MainMenuPanelButtonsActionListener(2));
+		
 		jbSair=new JButton("Sair");
 		jbSair.setBounds(100,100,100,30);
-		jbSair.addActionListener(new MainMenuPanelButtonsActionListener(2));
+		jbSair.addActionListener(new MainMenuPanelButtonsActionListener(3));
 		
 		//Adicionando
-		add(jbInicio);add(jbNotas);add(jbSair);
+		add(jbInicio);add(jbNotas);add(jbPerfil);add(jbSair);
 		
-		setBounds(0,0,1280,720);
+		setBounds(0,0,1280,50);
 		setVisible(true);
 	}
 
