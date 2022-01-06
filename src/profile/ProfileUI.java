dcba0e20b1ba2656ce154cda96ad4a2a04a99250
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-01-06 14:37:24 -0300

    Negrito em ProfileUI e defaultUser corrigido

--- src/profile/ProfileUI.java
+++ src/profile/ProfileUI.java
@@ -1,11 +1,14 @@
 package profile;
 
 import javax.swing.*;
+import javax.swing.text.AttributeSet.FontAttribute;
+
 import mainMenu.MainMenuPanel;
 import java.awt.Font;
 
 public class ProfileUI extends JFrame{
-	private Font bodyFont=new Font("Arial", Font.PLAIN, 20);
+	private Font boldBodyFont=new Font("Arial", Font.BOLD, 20); //Font.PLAIN
+	private Font bodyFont=new Font("Arial", Font.PLAIN, 20); //Font.PLAIN
 	
 	private MainMenuPanel mainMenuPanel;
 	private JLabel profileImg,jlName,jlNameInput,jlSexo,jlSexoInput;
@@ -23,45 +26,45 @@ public class ProfileUI extends JFrame{
 		//Body
 		jlName=new JLabel("Nome:");
 		jlNameInput=new JLabel("Lucas Santos de Jesus");
-		jlName.setFont(bodyFont); jlNameInput.setFont(bodyFont);
-		jlName.setBounds(400,100,60,30);
-		jlNameInput.setBounds(461,100,400,30);
+		jlName.setFont(boldBodyFont); jlNameInput.setFont(bodyFont);
+		jlName.setBounds(400,100,68,30);
+		jlNameInput.setBounds(469,100,400,30);
 		
 		jlSexo=new JLabel("Sexo:");
 		jlSexoInput=new JLabel("Outros");
-		jlSexo.setFont(bodyFont); jlSexoInput.setFont(bodyFont);
-		jlSexo.setBounds(400,130,50,30);
-		jlSexoInput.setBounds(455,130,68,30);
+		jlSexo.setFont(boldBodyFont); jlSexoInput.setFont(bodyFont);
+		jlSexo.setBounds(400,130,68,30);
+		jlSexoInput.setBounds(463,130,68,30);
 		
 		JLabel jlIdade=new JLabel("Idade:");
 		JLabel jlIdadeInput=new JLabel("21");
-		jlIdade.setFont(bodyFont); jlIdadeInput.setFont(bodyFont);
+		jlIdade.setFont(boldBodyFont); jlIdadeInput.setFont(bodyFont);
 		jlIdade.setBounds(573,130,60,30);
-		jlIdadeInput.setBounds(632,130,50,30);
+		jlIdadeInput.setBounds(640,130,50,30);
 		
 		JLabel jlRG=new JLabel("RG:");
 		JLabel jlRGInput=new JLabel("11.111.111-1");
-		jlRG.setFont(bodyFont); jlRGInput.setFont(bodyFont);
-		jlRG.setBounds(400,160,36,30);
-		jlRGInput.setBounds(439,160,130,30);
+		jlRG.setFont(boldBodyFont); jlRGInput.setFont(bodyFont);
+		jlRG.setBounds(400,160,45,30);
+		jlRGInput.setBounds(447,160,130,30);
 		
 		JLabel jlCpf=new JLabel("CPF:");
 		JLabel jlCpfInput=new JLabel("111.111.111-11");
-		jlCpf.setFont(bodyFont); jlCpfInput.setFont(bodyFont);
-		jlCpf.setBounds(400,190,45,30);
-		jlCpfInput.setBounds(450,190,140,30);
+		jlCpf.setFont(boldBodyFont); jlCpfInput.setFont(bodyFont);
+		jlCpf.setBounds(400,190,58,30);
+		jlCpfInput.setBounds(458,190,140,30);
 		
 		JLabel jlSerie=new JLabel("Série/Ano:");
 		JLabel jlSerieInput=new JLabel("9º");
-		jlSerie.setFont(bodyFont); jlSerieInput.setFont(bodyFont);
-		jlSerie.setBounds(400,220,92,30);
-		jlSerieInput.setBounds(495,220,28,30);
+		jlSerie.setFont(boldBodyFont); jlSerieInput.setFont(bodyFont);
+		jlSerie.setBounds(400,220,104,30);
+		jlSerieInput.setBounds(511,220,28,30);
 		
 		JLabel jlEtapa=new JLabel("Etapa:");
 		JLabel jlEtapaInput=new JLabel("Fundamental");
-		jlEtapa.setFont(bodyFont); jlEtapaInput.setFont(bodyFont);
-		jlEtapa.setBounds(400,250,58,30);
-		jlEtapaInput.setBounds(463,250,127,30);
+		jlEtapa.setFont(boldBodyFont); jlEtapaInput.setFont(bodyFont);
+		jlEtapa.setBounds(400,250,68,30);
+		jlEtapaInput.setBounds(471,250,127,30);
 		
 		//Adicionando
 		getContentPane().add(profileImg);
