ce884cf9b503ce7e853e81a4c7a7ef873544831f
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-01-11 17:09:58 -0300

    Tentando corrigir a falta deste pequeno coment

--- src/dbTools/DBMain.java
+++ src/dbTools/DBMain.java
@@ -20,7 +20,7 @@ public class DBMain {
 			String sql="select nome from teste where testeId=1;";
 			PreparedStatement ps1=conexao.prepareStatement(sql);
 			ResultSet rs1=ps1.executeQuery();
-			//LEMBRE-SE QUE O NEXT Ã‰ NECESSÃ?RIO!
+			//Lembre-se que o next é necessário!
 			rs1.next();
 			String nome=rs1.getString(1);
 			JOptionPane.showMessageDialog(null,"Nome: "+nome);
