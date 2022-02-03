ea17cfb34d56b65712f2e4458c0cad22b8d2f547
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-02-03 15:54:48 -0300

    ProfProfileUI 2

--- src/professor/profile/ProfProfileUI.java
+++ src/professor/profile/ProfProfileUI.java
@@ -1,5 +1,7 @@
 package professor.profile;
 
+import javax.swing.JLabel;
+
 import professor.mainMenu.ProfMainMenuPanel;
 import profile.*;
 
@@ -8,8 +10,18 @@ public class ProfProfileUI extends ProfileUIMasterClass{
 	public ProfProfileUI() {
 		super();
 		
+		JLabel jlGraduacao=new JLabel("Graduação:");
+		JLabel jlGraduacaoInput=new JLabel("");
+		jlGraduacao.setFont(getBoldBodyFont()); jlGraduacaoInput.setFont(getBodyFont());
+		jlGraduacao.setBounds(400,220,120,30);
+		jlGraduacaoInput.setBounds(522,220,250,30);
+		
+		//Adicionando
+		add(jlGraduacao); add(jlGraduacaoInput);
+		
 		//ProfMainMenuPanel
 		remove(getMainMenuPanel());
+		setMainMenuPanel(null);
 		profMainMenuPanel=new ProfMainMenuPanel();
 		add(profMainMenuPanel);
 	}
