9fbbb06417687a9695780a5bc9c4234d179686c0
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-02-02 21:35:50 -0300

    MainMenuPanelMasterClass

--- src/professor/mainMenu/ProfHomeMenuUI.java
+++ src/professor/mainMenu/ProfHomeMenuUI.java
@@ -1,19 +1,37 @@
 package professor.mainMenu;
 
+import java.awt.Font;
+
+import javax.swing.*;
+
 import main.Main;
 import mainMenu.*;
 
-public class ProfHomeMenuUI extends HomeMenuUI{
+public class ProfHomeMenuUI extends JFrame{
 	private ProfMainMenuPanel profMainMenuPanel;
+	private JLabel jlWelcome;
 	
-	public ProfHomeMenuUI(){
-		super();
-		getJlWelcome().setText("Bem vindo, Professor!");
-		getJlWelcome().setBounds(1280/2-220,720/2-200,600,30);
+	public ProfHomeMenuUI() {
+		super("Accendo");
 		
-		//ProfMainMenuPanel
-		remove(getMainMenuPanel());
+		jlWelcome=new JLabel("Bem vindo, Professor!");
+		jlWelcome.setBounds(1280/2-220,720/2-200,600,30);
+		jlWelcome.setFont(new Font("Lucida Handwriting", Font.PLAIN, 40));
+
+		//Adicionando
+		add(jlWelcome);
+		
+		//Adicionando mainMenuPanel
 		profMainMenuPanel=new ProfMainMenuPanel();
 		add(profMainMenuPanel);
+		
+		setSize(1280,720);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+		setVisible(true);
+
 	}
+	
+	
 }
