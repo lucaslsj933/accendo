9f68ac71bd8df281cb08bfc3b3c49ce53e7cb0e7
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-02-10 16:48:31 -0300

    ProfInsertUI quase finalizado

--- src/professor/insert/ProfInsertUI.java
+++ src/professor/insert/ProfInsertUI.java
@@ -13,9 +13,27 @@ import professor.mainMenu.ProfMainMenuPanel;
 
 public class ProfInsertUI extends BigTableUIMasterClass{
 	private ProfMainMenuPanel profMainMenuPanel;
+	private JLabel jlTurma;
+	private JComboBox jcbTurma;
+	private JButton jbLoadTurma;
 	
 	public ProfInsertUI() {
 		setTitle("Accendo - Inserir Notas");
+		
+		//Turma Select
+		
+		jlTurma=new JLabel("Turma: ");
+		jlTurma.setBounds(50,60,100,30);
+		jlTurma.setFont(getBodyFont());
+		
+		String dataJcbTurma[]={"Turma 1","Turma 2","Turma 3","Turma 4","Turma 5"};
+		jcbTurma=new JComboBox(dataJcbTurma);
+		jcbTurma.setBounds(120,50,400,50);
+		
+		jbLoadTurma=new JButton("Carregar Turma");
+		jbLoadTurma.setBounds(540,50,300,50);
+		
+		//Table
 		
 		String colunas[]={"Aluno","TA1","TA2","TA3","TA4"};
 		String data[][]=new String[30][5];
@@ -24,7 +42,9 @@ public class ProfInsertUI extends BigTableUIMasterClass{
 			data[i][0]="Aluno "+(String.valueOf(i+1));
 			data[i][1]=""; data[i][2]=""; data[i][3]=""; data[i][4]="";
 		}
-
+		
+		//Definindo tamanho, posição e inserindo dados
+		getJsp().setBounds(50,120,1150,550);
 		getTable().setModel(new DefaultTableModel(data, colunas));
 		
 		TableColumn tb0=getTable().getColumnModel().getColumn(0);
@@ -35,10 +55,29 @@ public class ProfInsertUI extends BigTableUIMasterClass{
 		tb0.setMaxWidth(800);
 		tb1.setMaxWidth(100); tb2.setMaxWidth(100); tb3.setMaxWidth(100); tb4.setMaxWidth(100);
 		
+		//Adicionando (além da super-classe)
+		add(jlTurma);add(jcbTurma);add(jbLoadTurma);
+		
 		//ProfMainMenuPanel
 		remove(getMainMenuPanel());
 		setMainMenuPanel(null);
 		profMainMenuPanel=new ProfMainMenuPanel();
 		add(profMainMenuPanel);
+	}
+
+	public ProfMainMenuPanel getProfMainMenuPanel() {
+		return profMainMenuPanel;
+	}
+
+	public void setProfMainMenuPanel(ProfMainMenuPanel profMainMenuPanel) {
+		this.profMainMenuPanel = profMainMenuPanel;
+	}
+
+	public JComboBox getJcbTurma() {
+		return jcbTurma;
+	}
+
+	public void setJcbTurma(JComboBox jcbTurma) {
+		this.jcbTurma = jcbTurma;
 	}
 }
