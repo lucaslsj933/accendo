0203ca84d767a5750cceb24b6e13f82d44799fa9
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-08-26 17:07:15 -0300

    Início de LucasNotasContainer

---
+++ src/consultar/LucasNotasContainer.java
@@ -0,0 +1,10 @@
+package consultar;
+
+//Por enquanto isto guarda apenas as notas de uma matéria!
+
+public class LucasNotasContainer {
+	//Se a matéria tem quatro notas em um ano, coloque 4 aqui...
+	private Nota[] nota=new Nota[4];
+	
+	
+}
