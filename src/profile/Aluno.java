c1b8184b557e0d3aa657b61df6c627ea9cfe073a
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2022-08-30 16:15:52 -0300

    Classe aluno

--- src/profile/Aluno.java
+++ src/profile/Aluno.java
@@ -1,5 +1,33 @@
 package profile;
 
-public class Aluno {
-
+public class Aluno extends Pessoa{
+	private int idAluno;
+	private String etapa;
+	private int serie;
+	private int turma_idTurma;
+	
+	public int getIdAluno() {
+		return idAluno;
+	}
+	public void setIdAluno(int idAluno) {
+		this.idAluno = idAluno;
+	}
+	public String getEtapa() {
+		return etapa;
+	}
+	public void setEtapa(String etapa) {
+		this.etapa = etapa;
+	}
+	public int getSerie() {
+		return serie;
+	}
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	public int getTurma_idTurma() {
+		return turma_idTurma;
+	}
+	public void setTurma_idTurma(int turma_idTurma) {
+		this.turma_idTurma = turma_idTurma;
+	}
 }
